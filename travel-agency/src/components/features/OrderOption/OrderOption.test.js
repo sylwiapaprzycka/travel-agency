118e623e72d8b1e8606255551260f24d0380895c
Sylwia Paprzycka <sylwia.paprzycka@gmail.com>
2020-09-06 12:38:33 +0200

    Add test to OrderOption

---
+++ travel-agency/src/components/features/OrderOption/OrderOption.test.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import OrderOption from './OrderOption';
+import DatePicker from 'react-datepicker';
+
+describe('Component OrderOption', () => {
+  it ('should render without crashing', () => {
+    const component = shallow(<OrderOption name='Lorem ipsum' type='Lorem ipsum' />);
+    expect(component).toBeTruthy();
+  });
+  it('should return empty object if called without required props', () => {
+    const component = shallow(<OrderOption />);
+    expect(component).toEqual({});
+  });
+  it('should render correct title', () => {
+    const expectedTitle = 'Lorem ipsum';
+    const expectedType ='dropdown';
+    const component = shallow(<OrderOption name={expectedTitle} type={expectedType} />);
+
+    const renderedTitle = (component.find('.title').text());
+    expect(renderedTitle).toEqual(expectedTitle);
+  });
+  const optionTypes = {
+    dropdown: 'OrderOptionDropdown',
+    icons: 'OrderOptionIcons',
+    checkboxes: 'OrderOptionCheckboxes',
+    number: 'OrderOptionNumber',
+    text: 'OrderOptionText',
+    date: 'OrderOptionDate',
+  };
+
+  const mockProps = {
+    id: 'abc',
+    name: 'Lorem',
+    values: [
+      {id: 'aaa', icon: 'h-square', name: 'Lorem A', price: 0},
+      {id: 'xyz', icon: 'h-square', name: 'Lorem X', price: 100},
+    ],
+    required: false,
+    currentValue: 'aaa',
+    price: '50%',
+    limits: {
+      min: 0,
+      max: 6,
+    },
+  };
+
+  const mockPropsForType = {
+    dropdown: {},
+    icons: {},
+    checkboxes: {currentValue: [mockProps.currentValue]},
+    number: {currentValue: 1},
+    text: {},
+    date: {},
+  };
+
+  const testValue = mockProps.values[1].id;
+  const testValueNumber = 3;
+
+  for(let type in optionTypes){
+    describe(`Component OrderOption with type=${type}`, () => {
+      /* test setup */
+      let component;
+      let subcomponent;
+      let renderedSubcomponent;
+      let mockSetOrderOption; /* 1 */
+
+      beforeEach(() => {
+        mockSetOrderOption = jest.fn(); /* 2 */
+        component = shallow(
+          <OrderOption
+            type={type}
+            setOrderOption={mockSetOrderOption} /* 3 */
+            {...mockProps}
+            {...mockPropsForType[type]}
+          />
+        );
+        subcomponent = component.find(optionTypes[type]);
+        renderedSubcomponent = subcomponent.dive();
+      });
+
+      /* common tests */
+      it(`renders ${optionTypes[type]}`, () => {
+        expect(subcomponent).toBeTruthy();
+        expect(subcomponent.length).toBe(1);
+      });
+
+      /* type-specific tests */
+      switch (type) {
+        case 'dropdown': {
+          /* tests for dropdown */
+          it('contains select and options', () => {
+            const select = renderedSubcomponent.find('select');
+            expect(select.length).toBe(1);
+
+            const emptyOption = select.find('option[value=""]').length;
+            expect(emptyOption).toBe(1);
+
+            const options = select.find('option').not('[value=""]');
+            expect(options.length).toBe(mockProps.values.length);
+            expect(options.at(0).prop('value')).toBe(mockProps.values[0].id);
+            expect(options.at(1).prop('value')).toBe(mockProps.values[1].id);
+          });
+          it('should run setOrderOption function on change', () => {
+            renderedSubcomponent.find('select').simulate('change', {currentTarget: {value: testValue}});
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+          });
+          break;
+        }
+        case 'icons': {
+          it('contains div with class icon', () => {
+            const icon = renderedSubcomponent.find('.icon');
+            expect(icon.length).toBe(3);
+            expect(icon.find('Icon')).toBeTruthy();
+          });
+          it('should run setOrderOption function on change', () => {
+            renderedSubcomponent.find('.icon').at(2).simulate('click');
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+          });
+          break;
+        }
+        case 'checkboxes': {
+          it('contains div with class checkboxes', () => {
+            const checkbox = renderedSubcomponent.find('.checkboxes');
+            expect(checkbox.length).toBe(1);
+
+          });
+          it('should run setOrderOption function on change', () => {
+            renderedSubcomponent.find(`[value="${[testValue]}"]`).simulate('change', {currentTarget: {checked: true}});
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: [mockProps.currentValue, testValue]});
+          });
+          break;
+        }
+        case 'number': {
+          it('contains input', () => {
+            const input = renderedSubcomponent.find('input');
+            expect(input.length).toBe(1);
+            expect(input.prop('type')).toBe('number');
+            expect(input.prop('value')).toBe(1);
+            expect(input.prop('min')).toBe(mockProps.limits.min);
+            expect(input.prop('max')).toBe(mockProps.limits.max);
+          });
+          it('should run setOrderOption function on change', () => {
+            renderedSubcomponent.find('input').simulate('change', {currentTarget: {value: testValueNumber}});
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValueNumber });
+          });
+          break;
+        }
+        case 'text': {
+          it('contains input', () => {
+            const input = renderedSubcomponent.find('input');
+            expect(input.length).toBe(1);
+            expect(input.prop('type')).toBe('text');
+            expect(input.prop('value')).toBe(mockProps.values[0].id);
+          });
+          it('should run setOrderOption function on change', () => {
+            renderedSubcomponent.find('input').simulate('change', {currentTarget: {value: testValue}});
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+          });
+          break;
+        }
+        case 'date': {
+          it('contains DatePicker', () => {
+            const datePicker = renderedSubcomponent.find(DatePicker);
+            expect(datePicker.length).toBe(1);
+
+          });
+          it('should run seontOrderOption function on change', () => {
+            renderedSubcomponent.find(DatePicker).simulate('change', testValue);
+            expect(mockSetOrderOption).toBeCalledTimes(1);
+            expect(mockSetOrderOption).toBeCalledWith({ [mockProps.id]: testValue });
+          });
+          break;
+        }
+      }
+    });
+  }
+});
--- travel-agency/src/components/features/OrderOption/OrderOptionDate.js
+++ travel-agency/src/components/features/OrderOption/OrderOptionDate.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
 
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-class OrderOptionDate extends React.Component {
-  state = {
-    startDate: new Date(),
-  };
+//ZMIANA Z POWODU TESTU
+// class OrderOptionDate extends React.Component {
+//   state = {
+//     startDate: new Date(),
+//   };
 
-  handleChange = date => {
-    this.setState({
-      startDate: date,
-    });
-  };
+//   handleChange = date => {
+//     this.setState({
+//       startDate: date,
+//     });
+//   };
 
-  render() {
-    return (
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-      />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <DatePicker
+//         selected={this.state.startDate}
+//         onChange={this.handleChange}
+//       />
+//     );
+//   }
+// }
+const OrderOptionDate = ({ currentValue, setOptionValue }) => (
+  <DatePicker
+    className={styles.input}
+    type="date"
+    value={currentValue}
+    selected={currentValue}
+    onChange={setOptionValue}
+    placeholderText={'Select a date'}
+  />
+);
 
+OrderOptionDate.propTypes = {
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.any,
+};
 export default OrderOptionDate;
